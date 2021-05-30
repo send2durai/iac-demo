af2dbd0312ab74b8d4e6b6165d048f6cea8bd22d
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-05-30 14:06:07 +0530

    Update vpc.tf
    region updated in vpc tf file

--- vpc.tf
+++ vpc.tf
@@ -3,6 +3,7 @@
 resource "aws_vpc" "main" {
   cidr_block       = "192.168.0.0/16"
   instance_tenancy = "default"
+  region           = "ap-south-1"
 
   tags             = {
     Name           = "demo-vpc"
