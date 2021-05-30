520b4a0421a5b4ded96fe958952814c7a9c91b6a
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-05-30 14:46:10 +0530

    Update vpc.tf
    subnet, vpc updated

--- vpc.tf
+++ vpc.tf
@@ -16,3 +16,36 @@ resource "aws_vpc" "main" {
     purpose        = "Terraform-CICD"
   }
 }
+
+## Creating a Pub/Private subnets
+resource "aws_subnet" "private-subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.0.0/24"
+
+  tags       = {
+    Name     = "Private-Subnet"
+    purpose  = "Connect-private EC2 instance"
+  }
+}
+resource "aws_subnet" "public-subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.1.0/24"
+
+  tags       = {
+    Name     = "Public-Subnet"
+    purpose  = "Connect-public EC2 instance"
+  }
+}
+
+### Creating a S3 bucket
+  
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-demo-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My demo-buckt"
+    Environment = "Dev"
+    purpose     = "Hosting a static web-content"
+  }
+}
