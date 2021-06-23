f9efb431a0988a258869d58be6011f64918b319d
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-06-23 09:12:16 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,22 +1,19 @@
 output "web-internet" {
-    value = "${aws_instance.web-internet.public_ip}"
+    value = "${aws_instance.web-internet.id}"
 }
 output "web-intranet" {
-    value = "${aws_instance.web-intranet.public_ip}"
+    value = "${aws_instance.web-intranet.id}"
 }
 
 output "demo-mysqldb" {
-    value  = "${aws_db_instance.demo-mysqldb}"
+    value  = "${aws_db_instance.demo-mysqldb.id}"
 }
 output "b" {
-    value = "${aws_s3_bucket.my-tf-demo-bucket}"
-}
-output "private-subnet" {
-    value = "${aws_subnet.private-id}"
+    value = "${aws_s3_bucket.my-tf-demo-bucket.id}"
 }
 output "public-subnet" {
-    value = "${aws_subnet.public-id}"
+    value = "${aws_subnet.public-subnet.id}"
 }
 output "vpc_id" {
-    value = "${aws_vpc.default.id}"
+    value = "${aws_vpc.dev.id}"
 }
