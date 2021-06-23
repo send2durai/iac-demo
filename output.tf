2336265ffe0a8271eac31f5d0e4a9a0004237edd
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-06-23 09:07:25 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,22 +1,22 @@
-output "public_ip" {
+output "web-internet" {
     value = "${aws_instance.web-internet.public_ip}"
 }
 output "public_ip" {
     value = "${aws_instance.web-intranet.public_ip}"
 }
 
-output "aws_db_instance" {
+output "demo-mysqldb" {
     value  = "${aws_db_instance.demo-mysqldb}"
 }
-output "aws_s3_bucket" {
+output "b" {
     value = "${aws_s3_bucket.my-tf-demo-bucket}"
 }
-output "aws_subnet" {
-    value = "${aws_subnet.private-subnet}"
+output "subnet" {
+    value = "${aws_subnet.private-id}"
 }
-output "aws_subnet" {
-    value = "${aws_subnet.public-subnet}"
+output "subnet" {
+    value = "${aws_subnet.public-id}"
 }
-output "aws_vpc" {
-    value = "${aws_vpc.dev}"
+output "vpc_id" {
+    value = "${aws_vpc.default.id}"
 }
