6953915ace995fcdff9fc7d88bdeca2bb0c10f73
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-06-23 09:18:39 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,5 @@
-output "web-internet" {
-    value = "${aws_instance.web-internet.id}"
-}
 output "demo-mysqldb" {
     value  = "${aws_db_instance.demo-mysqldb.id}"
-}
-output "b" {
-    value = "${aws_s3_bucket.my-tf-demo-bucket.id}"
 }
 output "public-subnet" {
     value = "${aws_subnet.public-subnet.id}"
