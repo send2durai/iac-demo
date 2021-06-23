cec52ec9d7d4e9404e17660d8c74e5f25b8a3bba
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-06-23 08:57:51 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -93,26 +93,3 @@ resource "aws_db_instance" "demo-mysqldb" {
   publicly_accessible  = true
   skip_final_snapshot  = true
 }
-
-output "public_ip" {
-    value = "${aws_instance.web-internet.public_ip}"
-}
-output "public_ip" {
-    value = "${aws_instance.web-intranet.public_ip}"
-}
-
-output "aws_db_instance" {
-    value  = "${aws_db_instance.demo-mysqldb}"
-}
-output "aws_s3_bucket" {
-    value = "${aws_s3_bucket.my-tf-demo-bucket}"
-}
-output "aws_subnet" {
-    value = "${aws_subnet.private-subnet}"
-}
-output "aws_subnet" {
-    value = "${aws_subnet.public-subnet}"
-}
-output "aws_vpc" {
-    value = "${aws_vpc.dev}"
-}
