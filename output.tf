6a94ed3ef7dd09d4764682550f8152ce174eb2bf
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-06-23 10:03:23 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,16 +2,8 @@ output "VPC_ID" {
     value = aws_vpc.dev.id 
 }
 
-output "DEF_VPC_ID" {
-    value = var.DEF_VPC_ID
-}
-
 output "VPC_CIDR" {
     value = var.VPC_CIDR 
-}
-
-output "DEFAULT_VPC_CIDR" {
-    value = data.aws_vpc.default.cidr_block
 }
 
 output "PRIVATE-SUBNET" {
