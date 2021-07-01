3c5eefed814292936a1f9aea9c1b01d3d6850b00
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-07-01 18:01:24 +0530

    Update and rename vpc.tf to resource.tf
    Initial commit

---
+++ resource.tf
@@ -0,0 +1,40 @@
+## Configure the provider block
+provider "aws" {
+  region        =var.aws_region
+}
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+## Configure the resource block
+resource "aws_instance" "web-server-1" {
+  ami                       = var.aws_ami_id
+  instance_type             = var.aws_instance_type[0]
+  subnet_id                 = var.aws_subnet_id[0]
+  vpc_security_group_ids    = [var.vpc_security_group_ids[0]]
+  key_name                  = var.aws_key_name
+
+  tags                      = {
+    Name                    = var.aws_tag_name[0]
+    Purpose                 = var.aws_tag_purpose[0]
+    Env                     = var.aws_resource_env[0]
+  }
+}
+## configure the resoruce block with another instance_type
+resource "aws_instance" "web-server-2" {
+  ami                       = var.aws_ami_id
+  instance_type             = var.aws_instance_type[1]
+  subnet_id                 = var.aws_subnet_id[1]
+  vpc_security_group_ids    = [var.vpc_security_group_ids[1]]
+  key_name                  = var.aws_key_name
+
+  tags                      = {
+    Name                    = var.aws_tag_name[1]
+    Purpose                 = var.aws_tag_purpose[1]
+    Env                     = var.aws_resource_env[2]
+  }
+}
