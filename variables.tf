e40f49eb8894328c254d4f2c9f194a904f322916
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-07-08 10:19:06 +0530

    Update variables.tf
    initial commit

--- variables.tf
+++ variables.tf
@@ -2,18 +2,20 @@
 variable "aws_region" {
   type   = string
 }
-## Declaring aws keys in a block
-## variable "aws_access_key" {}
-## variable "aws_secret_key" {}
-
 # Declaring the environmetnal variables
-variable "aws_resource_env" {}
+variable "aws_resource_env" {
+  type     = list
+}
 
 # Declaring the key_name for connecting ec2 instances
-variable "aws_key_name" {}
+variable "aws_key_name" {
+  type    = string
+}
 
 # Declaring the instnace type
-variable "aws_instance_type" {}
+variable "aws_instance_type" {
+  type    = list
+}
 
 # Declaring the subnet id
 variable "aws_subnet_id" {
@@ -22,7 +24,7 @@ variable "aws_subnet_id" {
 
 # Declaring the ami_id
 variable "aws_ami_id" {
-  type  = list
+  type  = string
 }
 
 # Declaring the security_group_id
@@ -31,50 +33,16 @@ variable "vpc_security_group_ids" {
 }
 
 # Declaring the tag name
-variable "aws_tag_name" {}
+variable "aws_tag_name" {
+  type  = list
+}
 
 # Declaring in the tag for Purpose
-variable "aws_tag_purpose" {}
-######################################################################## CLB ###################################################################
-
-## Declaring variables in this block
-#variable "aws_region" {
-  #type   = string
-#}
-## Declaring aws keys in a block
-#variable "aws_access_key" {}
-#variable "aws_secret_key" {}
-
-# Declaring the resource name in the resource block
-variable "clb_name" {
-  type     = string
-}
-
-# Declaring the availability_zone in resoruce block
-variable "avail_zone" {
-  type     = list
-}
-
-# Declaring the ideal_timeout in resource block
-variable "ideal_timeout" {
-  type     = number
-}
-
-# Declaring the connection_draining in resource block
-variable "connection_draining" {
-  type     = bool
-}
-# Declaring Tag name variables
-variable "aws_tag_name" {
-  type     = list
-}
-
-# Declaring the tag environmetnal variables
-variable "aws_tag_env" {
-  type      = list
-}
-
-# Declaring the tag Purpose
 variable "aws_tag_purpose" {
-  type      = list
+  type  = list
+}
+
+# Declaring in the tag for Purpose
+variable "aws_tag_env" {
+  type  = list
 }
